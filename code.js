3b54738efc74c645a9e0d0f0457f4308334c57ae
DaShinja <byron@dashinja.com>
2019-04-08 17:35:54 -0400

    Can change shape of paintbrush used

--- code.js
+++ code.js
@@ -1,8 +1,12 @@
+// Variables
 const canvas = document.querySelector('.canvas')
 const clearButton = document.querySelector('.clear')
+const shapes = document.querySelector('.shapes')
 
 let isDrawing = false
+let shape = 'circleOption'
 
+// Event Listeners
 canvas.addEventListener('mousedown', e => {
   console.log('Drawing')
   isDrawing = true
@@ -23,15 +27,22 @@ canvas.addEventListener('mousemove', e => {
   if (isDrawing) paintSpot(e.pageX, e.pageY)
 })
 
+clearButton.addEventListener('click', () => {
+  canvas.innerHTML = ''
+})
+
+shapes.addEventListener('click', e => {
+  if (e.target.className !== 'shapes') {
+    shape = e.target.className
+  }
+})
+
+//Functions
 function paintSpot(x, y) {
   const spot = document.createElement('div')
-  spot.className = 'spot circleOption'
+  spot.className = `spot ${shape}`
   spot.style.left = `${x - 22}px`
   spot.style.top = `${y - 22}px`
   spot.style.background = 'lightblue'
   canvas.appendChild(spot)
 }
-
-clearButton.addEventListener('click', () => {
-  canvas.innerHTML = ''
-})
